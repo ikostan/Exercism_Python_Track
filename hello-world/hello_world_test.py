df69ac427a03b1ec4fb0ed31135cf5eb0b7048dd
ikostan <igorkostan@gmail.com>
2019-06-10 16:40:17 -0700

    Update hello_world_test.py

--- hello-world/hello_world_test.py
+++ hello-world/hello_world_test.py
@@ -1,6 +1,15 @@
-def hello():
-    return "Hello, World!"
+import unittest
+
+import hello_world
 
 
-def test_set_comparison():
-    assert hello() == "Hello, World!"
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.1.0
+
+class HelloWorldTest(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello_world.hello(), 'Hello, World!')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
