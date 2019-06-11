5bec5ba096725ad8828c0204db08aea37ae7ac81
ikostan <igorkostan@gmail.com>
2019-06-10 17:54:09 -0700

    Robot Name

---
+++ robot-name/robot_name.py
@@ -0,0 +1,25 @@
+class Robot(object):
+    def __init__(self):
+        self.name = ''
+        self.reset()
+
+    def reset(self):
+        '''
+        Every once in a while we need to reset a robot
+        to its factory settings, which means that their
+        name gets wiped. The next time you ask,
+        it will respond with a new random name.
+        :return:
+        '''
+        import string
+        import random
+        old_name = self.name
+        while old_name == self.name:
+            self.name = ''
+            for n in range(0, 2):
+                self.name += string.ascii_uppercase[random.randint(0, 25)]
+
+            for n in range(0, 3):
+                self.name += str(random.randint(0, 9))
+
+
