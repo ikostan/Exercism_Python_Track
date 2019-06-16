621f749c470ad7df05eb0364ccd1c29710bdc183
ikostan <igorkostan@gmail.com>
2019-06-16 15:50:09 -0700

    Phone Number -> main class refactoring

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -1,41 +1,21 @@
 class Phone(object):
 
     def __init__(self, phone_number):
-        #  print('Raw data: {0}'.format(phone_number))  # debug only
-
-        npa = [200, 999]
         digits_only = ''.join(n for n in phone_number if n.isdigit())
-        #  print('Formatted number: {0}'.format(digits_only))  # debug only
 
-        if len(digits_only) == 10:
-            #  print('NPA: {0}'.format(digits_only[0:3]))  # debug only
-            #  NPA validation
-            if npa[0] <= int(digits_only[0:3]) <= npa[1]:
-                #  print('Exchange code: {0}'.format(digits_only[3]))  # debug only
-                # exchange code validation
-                if int(digits_only[3]) > 1:
-                    self.number = digits_only
-                    self.area_code = digits_only[0:3]
-                else:
-                    raise ValueError('Invalid exchange code')
-            else:
-                raise ValueError('Invalid NPA')
-        elif digits_only[0] == '1' and len(digits_only[1:]) == 10:
-            #  print('NPA: {0}'.format(digits_only[1:4]))  # debug only
-            #  NPA validation
-            if npa[0] <= int(digits_only[1:4]) <= npa[1]:
-                # print('Exchange code: {0}'.format(digits_only[4]))  # debug only
-                # exchange code validation
-                if int(digits_only[4]) > 1:
-                    self.number = digits_only[1:]
-                    self.area_code = digits_only[1:4]
+        if self.__get_country_code__(digits_only):
+            if self.__npa__(digits_only):
+                if self.__exchange_code__(digits_only):
+                    self.number = digits_only if len(digits_only) == 10 else digits_only[1:]
+                    self.area_code = self.__npa__(digits_only)
                 else:
                     raise ValueError('Invalid exchange code')
             else:
                 raise ValueError('Invalid NPA')
         else:
-            raise ValueError('Invalid phone number')
+            raise ValueError('Invalid country code')
 
+    # return formatted phone number:
     def pretty(self):
         if len(self.number) == 10:
             return '({0}) {1}-{2}'.format(self.number[0:3],
@@ -45,4 +25,34 @@ class Phone(object):
             return '({0}) {1}-{2}'.format(self.number[1:3],
                                           self.number[4:7],
                                           self.number[7:])
+
+    # Country code validation:
+    def __get_country_code__(self, digits_only: str):
+
+        if len(digits_only) == 10:
+            return digits_only[0:3]
+        elif digits_only[0] == '1' and len(digits_only[1:]) == 10:
+            return digits_only[1:4]
+        else:
+            return False
+
+    # NPA validation:
+    def __npa__(self, digits_only: str):
+        npa = [200, 999]
+
+        if npa[0] <= int(digits_only[0:3]) <= npa[1] and len(digits_only) == 10:
+            return digits_only[0:3]
+        elif npa[0] <= int(digits_only[1:4]) <= npa[1] and len(digits_only) == 11:
+            return digits_only[1:4]
+        else:
+            return False
+
+    # Area code validation:
+    def __exchange_code__(self, digits_only: str):
+        if int(digits_only[3]) > 1 and len(digits_only) == 10:
+            return True
+        elif int(digits_only[4]) > 1 and len(digits_only) == 11:
+            return True
+        else:
+            return False
 
