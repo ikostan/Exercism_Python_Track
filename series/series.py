864c54763c7408bbb61262f0a2e8732b32fec559
ikostan <igorkostan@gmail.com>
2019-06-16 13:56:57 -0700

    Series -> done, all ValueErrors are implemented, all test OK

--- series/series.py
+++ series/series.py
@@ -1,3 +1,13 @@
 def slices(series, length):
-    return [series[start:length + start] for start in range(len(series))
-            if len(series[start:length + start]) == length]
+    if not series or series == '':
+        raise ValueError('Empty series is invalid')
+    elif length < 0:
+        raise ValueError('Length can not be negative')
+    elif length == 0:
+        raise ValueError('Length can not be zero')
+    elif length > len(series):
+        raise ValueError('Length is too large')
+    else:
+        return [series[start:length + start] for start in range(len(series))
+                if len(series[start:length + start]) == length]
+
