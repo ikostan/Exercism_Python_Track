2c5b614ca397948f08bc6b8ee3a21a2b116a50f6
ikostan <igorkostan@gmail.com>
2019-06-16 13:13:55 -0700

    RNA Transcription -> done, all test OK

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,9 @@
 def to_rna(dna_strand):
-    pass
+    dna = {'G': 'C',
+           'C': 'G',
+           'T': 'A',
+           'A': 'U'}
+
+    return ''.join(dna[char] for char in dna_strand.upper())
+
+
