99dab6db9cc5596e8f06031346de59497bf9e7ef
ikostan <igorkostan@gmail.com>
2019-06-20 10:52:41 -0700

    Update high_scores.py

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -7,8 +7,18 @@ def personal_best(scores):
 
 
 def personal_top_three(scores):
+    '''
+    Old solution:
+
     if len(scores) >= 3:
         start = len(scores) - 3
         return sorted(sorted(scores)[start:], reverse=True)
     else:
         return sorted(scores, reverse=True)
+
+    :param scores:
+    :return:
+    '''
+
+    return sorted(scores, reverse=True)[:3]
+
