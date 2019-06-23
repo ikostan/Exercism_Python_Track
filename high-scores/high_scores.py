47090ecbb0547dc97abd6b2f55d6e991520eca7a
ikostan <igorkostan@gmail.com>
2019-06-23 13:28:38 -0700

    Update high_scores.py
    if scores is very large, then sorting it can take awhile. In that case,it is better to import heapq and return heapq.nlargest(3, scores)

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -7,18 +7,8 @@ def personal_best(scores):
 
 
 def personal_top_three(scores):
-    '''
-    Old solution:
-
-    if len(scores) >= 3:
-        start = len(scores) - 3
-        return sorted(sorted(scores)[start:], reverse=True)
-    else:
-        return sorted(scores, reverse=True)
-
-    :param scores:
-    :return:
-    '''
-
-    return sorted(scores, reverse=True)[:3]
+    # This module provides an implementation of the heap queue algorithm,
+    # also known as the priority queue algorithm.
+    import heapq
+    return heapq.nlargest(3, scores)
 
