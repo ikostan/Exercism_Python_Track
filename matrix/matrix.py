e1e6a540175aa4543639651a84aa3c41c77e4672
ikostan <igorkostan@gmail.com>
2019-07-14 00:17:51 -0700

    Consider trying the following:
    Use this as an opportunity to practice writing docstrings
    Also, you don't need (object) in your class declaration if you are using Python 3.6 or higher. object is inferred, so you only need to put in class name(s) if you're inheriting directly from them.

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,10 +1,24 @@
-class Matrix(object):
+class Matrix:
     def __init__(self, matrix_string):
+        '''
+        convert input string into 2D matrix, including parsing sub strings to integers
+        :param matrix_string:
+        '''
         self.matrix = [[int(number) for number in m.split(' ')] for m in matrix_string.split('\n')]
 
     def row(self, index):
+        '''
+        returns row by index
+        :param index:
+        :return:
+        '''
         return self.matrix[index - 1]
 
     def column(self, index):
+        '''
+        returns column by index
+        :param index:
+        :return:
+        '''
         return [m[index - 1] for m in self.matrix]
 
