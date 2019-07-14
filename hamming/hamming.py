3895f94ca327b070ca0a54f570ffc4cda232b66f
ikostan <igorkostan@gmail.com>
2019-07-14 00:42:55 -0700

    Update hamming.py
    Hamming >>> DONE

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,12 @@
 def distance(strand_a, strand_b):
-    pass
+
+    if len(strand_a) != len(strand_b):
+        raise ValueError('.+')
+
+    hamming_distance = 0
+    for i, s in enumerate(strand_a):
+        if s != strand_b[i]:
+            hamming_distance += 1
+
+    return hamming_distance
+
