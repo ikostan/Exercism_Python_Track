f810a3a0f3b063fd2c91aea1abc07367bac0c01a
ikostan <igorkostan@gmail.com>
2019-07-16 17:41:49 -0700

    Update difference_of_squares.py
    Doc stream added

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,12 +1,28 @@
 def square_of_sum(number):
+    '''
+    The square of the sum of the first n natural numbers
+    :param number:
+    :return:
+    '''
     number = list(range(1, number + 1))
     return sum(number) ** 2
 
 
 def sum_of_squares(number):
+    '''
+    The sum of the squares of the first n natural numbers
+    :param number:
+    :return:
+    '''
     number = list(range(1, number + 1))
     return sum([(n ** 2) for n in number])
 
 
 def difference_of_squares(number):
+    '''
+    The difference between the square of the sum of the first n natural numbers
+    and the sum of the squares of the first n natural numbers
+    :param number:
+    :return:
+    '''
     return square_of_sum(number) - sum_of_squares(number)
