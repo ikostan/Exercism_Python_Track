f189a339dd8fe6244b94effbcec273b0ace8d2e0
ikostan <igorkostan@gmail.com>
2019-07-16 17:38:54 -0700

    Update hamming.py
    Comments + doc stream added

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,8 +1,16 @@
 def distance(strand_a, strand_b):
+    '''
+    Calculate the Hamming Distance between two DNA strands.
+    :param strand_a:
+    :param strand_b:
+    :return:
+    '''
 
+    # Return error if DNA strands have different length:
     if len(strand_a) != len(strand_b):
         raise ValueError('.+')
 
+    # Calculate Hamming Distance:
     hamming_distance = 0
     for i, s in enumerate(strand_a):
         if s != strand_b[i]:
