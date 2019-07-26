72e71ed5983d688aea0b9a19cf358a2b68b23f90
Egor Kostan <igorkostan@gmail.com>
2019-07-26 14:05:27 -0700

    Update darts.py

--- darts/darts.py
+++ darts/darts.py
@@ -6,7 +6,6 @@ def score(x, y):
     :return:
     '''
 
-    points = 0
     radius = 10  # The outer circle has a radius of 10 units.
     landing_point = (x - 0)**2 + (y - 0)**2
 
@@ -16,7 +15,7 @@ def score(x, y):
 
     # If the dart lands in the outer circle of the target, player earns 1 point.
     # The middle circle a radius of 5 units.
-    if (radius / 2) ** 2 <= landing_point <= radius ** 2:
+    if (radius / 2) ** 2 < landing_point <= radius ** 2:
         points = 1
 
     # If the dart lands in the middle circle of the target, player earns 5 points.
