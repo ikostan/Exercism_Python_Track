628efc29a220e3e24ec784031a1154d8bda15935
Egor Kostan <igorkostan@gmail.com>
2019-08-16 00:31:11 -0700

    Create isogram.py

---
+++ isogram/isogram.py
@@ -0,0 +1,32 @@
+def is_isogram(string):
+    """
+    Determine if a word or phrase is an isogram.
+
+    An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter,
+    however spaces and hyphens are allowed to appear multiple times.
+
+    Examples of isograms:
+        lumberjacks
+        background
+        downstream
+        six-year-old
+
+    The word isograms, however, is not an isogram, because the s repeats.
+
+    :param string:
+    :return:
+    """
+
+    counter = 0
+
+    # 1. spaces allowed to appear multiple times
+    counter += string.count('-')
+
+    # 2. hyphens are allowed to appear multiple times
+    counter += string.count(' ')
+
+    # 3. No repeating letters
+    unique_letters_only = set([char for char in string if char.isalpha()])
+    counter += len(unique_letters_only)
+
+    return counter == len(string)
