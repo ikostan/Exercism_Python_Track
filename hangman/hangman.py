9b488e7e1ace75722ae1cc0dcff91a953506b782
Egor Kostan <igorkostan@gmail.com>
2019-08-20 15:40:21 -0700

    Create hangman.py

---
+++ hangman/hangman.py
@@ -0,0 +1,56 @@
+# Game status categories
+# Change the values as you see fit
+STATUS_WIN = "win"
+STATUS_LOSE = "lose"
+STATUS_ONGOING = "ongoing"
+
+
+class Hangman(object):
+    def __init__(self, word):
+        self.remaining_guesses = 9
+        self.status = STATUS_ONGOING
+        self.word = word
+        self.masked_word = '_' * len(word)
+
+    def guess(self, char):
+
+        if self.status != STATUS_ONGOING:
+            raise ValueError("ERROR: no guesses left")
+
+        if char in self.word and char not in self.masked_word:
+            chars = []
+            for c in self.word:
+                if c == char:
+                    chars.append(c)
+                else:
+                    chars.append('_')
+            new_word = ''.join(chars)
+
+            masked = ''
+            for i, c in enumerate(new_word):
+                if c != '_':
+                    masked += c
+                else:
+                    masked += self.masked_word[i]
+            self.masked_word = masked
+        else:
+            self.remaining_guesses -= 1
+
+        if self.masked_word == self.word:
+            self.status = STATUS_WIN
+
+        if self.remaining_guesses < 0:
+            self.status = STATUS_LOSE
+
+        if self.remaining_guesses > 0:
+            self.status = STATUS_ONGOING
+
+        print("{} : {}".format(self.masked_word, self.word))
+
+    def get_masked_word(self):
+        return self.masked_word
+
+    def get_status(self):
+        if self.masked_word == self.word:
+            self.status = STATUS_WIN
+        return self.status
