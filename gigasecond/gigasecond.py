500b01eefb5ccfca597294c39f8ebb24f4f3e83b
Egor Kostan <igorkostan@gmail.com>
2019-09-12 12:12:02 -0700

    Update gigasecond.py

--- gigasecond/gigasecond.py
+++ gigasecond/gigasecond.py
@@ -2,12 +2,12 @@ import datetime
 
 
 def add(moment: datetime):
-    '''
+    """
     Calculate the moment when someone has lived for 10^9 seconds.
     A gigasecond is 10^9 (1,000,000,000) seconds.
     :param moment:
     :return:
-    '''
+    """
 
     # Calculate how many days in one gigasecond
     days = (((10 ** 9) / 60) / 60) / 24
