ae076782e8d1e3926eb3729342cdf5c1f9d77a20
Egor Kostan <igorkostan@gmail.com>
2019-09-13 18:40:13 -0700

    Update pangram.py
    E231 missing whitespace after ':'

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,11 +1,13 @@
-def is_pangram(sentence:str):
-    '''
+def is_pangram(sentence: str):
+    """
     A pangram is a sentence using every letter of the alphabet at least once.
+
     The best known English pangram is:
             The quick brown fox jumps over the lazy dog.
+
     :param sentence:
     :return:
-    '''
+    """
     import string
     letters = string.ascii_lowercase
     sentence = sentence.lower()
