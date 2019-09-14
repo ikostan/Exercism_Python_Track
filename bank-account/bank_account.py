b70fc5f69109ecf69cd2481f8a80bd1ba53911e7
Egor Kostan <igorkostan@gmail.com>
2019-09-13 17:34:18 -0700

    Update bank_account.py
    E501 line too long (91 > 79 characters)

--- bank-account/bank_account.py
+++ bank-account/bank_account.py
@@ -2,9 +2,10 @@ import threading
 
 
 class BankAccount(object):
-    '''
-    Simulate a bank account supporting opening/closing, withdrawals, and deposits of money.
-    '''
+    """
+    Simulate a bank account supporting opening/closing,
+    withdrawals, and deposits of money.
+    """
 
     def __init__(self):
         self._balance = 0.0
@@ -12,19 +13,19 @@ class BankAccount(object):
         self._lock = threading.Lock()
 
     def get_balance(self):
-        '''
+        """
         Returns current balance
         :return:
-        '''
+        """
 
         self._check_is_account_opened()
         return self._balance
 
     def open(self):
-        '''
+        """
         Simulate a bank account supporting opening
         :return:
-        '''
+        """
 
         if not self._is_opened:
             self._is_opened = True
@@ -33,11 +34,11 @@ class BankAccount(object):
         return None
 
     def deposit(self, amount):
-        '''
+        """
         Clients can make deposits
         :param amount:
         :return:
-        '''
+        """
 
         self._check_is_account_opened()
         BankAccount._check_is_negative_amount(amount)
@@ -47,11 +48,11 @@ class BankAccount(object):
         return None
 
     def withdraw(self, amount):
-        '''
+        """
         Clients can make withdrawals
         :param amount:
         :return:
-        '''
+        """
 
         self._check_is_account_opened()
         BankAccount._check_is_negative_amount(amount)
@@ -64,10 +65,10 @@ class BankAccount(object):
         return None
 
     def close(self):
-        '''
+        """
         It should be possible to close an account
         :return:
-        '''
+        """
 
         self._check_is_account_opened()
         self._is_opened = False
@@ -75,10 +76,10 @@ class BankAccount(object):
         return None
 
     def _check_is_account_opened(self):
-        '''
+        """
         Operations against a closed account must fail
         :return:
-        '''
+        """
 
         if not self._is_opened:
             raise ValueError('ERROR: account is closed.')
@@ -86,11 +87,11 @@ class BankAccount(object):
 
     @staticmethod
     def _check_is_negative_amount(amount):
-        '''
+        """
         Cannot withdraw or deposit negative amount
         :param amount:
         :return:
-        '''
+        """
 
         if amount < 0:
             raise ValueError("ERROR: Cannot withdraw/deposit negative amount")
