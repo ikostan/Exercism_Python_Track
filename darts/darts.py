a1bb0fa57c1f6997650daa1611583eee32ef3058
Egor Kostan <igorkostan@gmail.com>
2019-09-13 17:35:42 -0700

    Update darts.py
    E501 line too long (102 > 79 characters)

--- darts/darts.py
+++ darts/darts.py
@@ -10,20 +10,24 @@ def score(x, y):
     radius = 10  # The outer circle has a radius of 10 units.
     landing_point = (x - 0)**2 + (y - 0)**2
 
-    # If the dart lands outside the target, player earns no points (0 points).
+    # If the dart lands outside the target,
+    # player earns no points (0 points).
     if landing_point > radius ** 2:
         points = 0
 
-    # If the dart lands in the outer circle of the target, player earns 1 point.
+    # If the dart lands in the outer circle of the target,
+    # player earns 1 point.
     # The middle circle a radius of 5 units.
     if (radius / 2) ** 2 < landing_point <= radius ** 2:
         points = 1
 
-    # If the dart lands in the middle circle of the target, player earns 5 points.
+    # If the dart lands in the middle circle of the target,
+    # player earns 5 points.
     if 1 ** 2 < landing_point <= (radius / 2) ** 2:
         points = 5
 
-    # If the dart lands in the inner circle of the target, player earns 10 points.
+    # If the dart lands in the inner circle of the target,
+    # player earns 10 points.
     # The inner circle a radius of 1
     if landing_point <= 1 ** 2:
         points = 10
