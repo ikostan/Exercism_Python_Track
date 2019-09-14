05f4dd9130a5d76a6bd8f09ac3f2116fce7112b6
Egor Kostan <igorkostan@gmail.com>
2019-09-13 17:34:06 -0700

    Update saddle_points.py
    E501 line too long (100 > 79 characters)

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,9 +1,9 @@
 def saddle_points(matrix):
-    '''
+    """
     Detect saddle points in a matrix.
     :param matrix:
     :return:
-    '''
+    """
 
     # Return empty dict in case matrix is empty
     if len(matrix) == 0:
@@ -25,9 +25,9 @@ def saddle_points(matrix):
         for i, c in enumerate(column):
             if c > min(column):
                 continue
+            # It's called a "saddle point" because it is greater than or equal
+            # to every element in its row and less than or equal to every element in its column.
             else:
-                # It's called a "saddle point" because it is greater than or equal
-                # to every element in its row and less than or equal to every element in its column.
                 if max(matrix[i]) == c:
                     answer.append({"row": i + 1, "column": col_i + 1})
 
