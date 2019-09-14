68a63305d44caadf95e5d316f107b772b69ec7a4
Egor Kostan <igorkostan@gmail.com>
2019-09-13 17:40:30 -0700

    Update grains.py
    E501 line too long (81 > 79 characters)

--- grains/grains.py
+++ grains/grains.py
@@ -1,23 +1,27 @@
+
+ERROR = 'ERROR: number should be between 1 and 64 (inclusive).'
+
+
 def square(number):
-    '''
+    """
     Calculates how many grains were on each square
     :param number:
     :return:
-    '''
+    """
 
     if number <= 0 or not number or number > 64:
-        raise ValueError('ERROR: number should be between 1 and 64 (inclusive).')
+        raise ValueError(ERROR)
 
     return 2**(number - 1)
 
 
 def total(number):
-    '''
+    """
     Calculates the total number of grains
     :param number:
     :return:
-    '''
+    """
     if number <= 0 or not number or number > 64:
-        raise ValueError('ERROR: number should be between 1 and 64 (inclusive).')
+        raise ValueError(ERROR)
 
     return 2 ** number - 1
