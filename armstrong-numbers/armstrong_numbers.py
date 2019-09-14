53df5846c2408097fb4e3e472d546e310b06022e
Egor Kostan <igorkostan@gmail.com>
2019-09-13 18:29:49 -0700

    Update armstrong_numbers.py
    E501 line too long (81 > 79 characters)

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,7 +1,9 @@
 def is_armstrong_number(number):
     """
-    Armstrong number is a number that is equal to the sum of cubes of its digits.
-    For example 0, 1, 153, 370, 371 and 407 are the Armstrong numbers.
+    Armstrong number is a number that is equal
+    to the sum of cubes of its digits.
+    For example 0, 1, 153, 370, 371
+    and 407 are the Armstrong numbers.
     Source: https://www.javatpoint.com/armstrong-number-in-c
 
     :param number:
