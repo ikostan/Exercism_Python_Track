b619b89e9e3876faacd4889d2b1fefb570acda07
Egor Kostan <igorkostan@gmail.com>
2019-09-13 17:48:42 -0700

    Update leap.py
    E231 missing whitespace after ':'

--- leap/leap.py
+++ leap/leap.py
@@ -1,12 +1,12 @@
-def leap_year(year:int):
-    '''
+def leap_year(year: int):
+    """
     The tricky thing here is that a leap year occurs:
         on every year that is evenly divisible by 4
             except every year that is evenly divisible by 100
                 except every year that is evenly divisible by 400
     :param year:
     :return:
-    '''
+    """
 
     if year % 4 == 0:
         if year % 100 == 0 and year % 400 == 0:
