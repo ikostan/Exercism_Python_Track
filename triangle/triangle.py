4ae4cc430390e30ca2dcd4b0979532acfc2dad5b
Egor Kostan <igorkostan@gmail.com>
2019-09-16 14:39:39 -0700

    Update triangle.py

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,9 +1,9 @@
 def equilateral(sides):
-    '''
+    """
     An equilateral triangle has all three sides the same length.
     :param sides:
     :return:
-    '''
+    """
 
     if not is_valid(sides):
         return False
@@ -15,13 +15,13 @@ def equilateral(sides):
 
 
 def isosceles(sides):
-    '''
+    """
     An isosceles triangle has at least two sides the same length.
     (It is sometimes specified as having exactly two sides the same length,
     but for the purposes of this exercise we'll say at least two.)
     :param sides:
     :return:
-    '''
+    """
 
     if not is_valid(sides):
         return False
@@ -33,11 +33,11 @@ def isosceles(sides):
 
 
 def scalene(sides):
-    '''
+    """
     A scalene triangle has all sides of different lengths.
     :param sides:
     :return:
-    '''
+    """
 
     if not is_valid(sides):
         return False
@@ -49,13 +49,13 @@ def scalene(sides):
 
 
 def is_valid(sides):
-    '''
+    """
     For a shape to be a triangle at all, all sides have to be of length > 0,
     and the sum of the lengths of any two sides must be greater than or equal
     to the length of the third side.
     :param sides:
     :return:
-    '''
+    """
 
     # 1 - all sides have to be of length > 0:
     if sum(sides) <= 0:
@@ -73,4 +73,3 @@ def is_valid(sides):
         return False
 
     return True
-
