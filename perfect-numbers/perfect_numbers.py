feb317b3f1d540a44af00554da58b6f060706049
Egor Kostan <igorkostan@gmail.com>
2019-09-16 13:44:53 -0700

    Update perfect_numbers.py

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,12 +1,16 @@
 def classify(number):
     """
     Determine if a number is perfect, abundant, or deficient
-    based on Nicomachus' (60 - 120 CE) classification scheme for natural numbers.
+    based on Nicomachus' (60 - 120 CE) classification scheme
+    for natural numbers.
 
-    Categories of perfect, abundant, or deficient based on their aliquot sum.
+    Categories of perfect, abundant, or deficient
+    based on their aliquot sum.
 
-    The aliquot sum is defined as the sum of the factors of a number
+    The aliquot sum is defined as the sum
+    of the factors of a number
     not including the number itself.
+
     For example, the aliquot sum of 15 is (1 + 3 + 5) = 9
 
     :param number:
