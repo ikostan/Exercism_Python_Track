0e2963c729a9c226c1928c30f8cfbe60d95b57aa
Egor Kostan <igorkostan@gmail.com>
2019-09-16 17:07:19 -0700

    Update anagram.py
    [C0111(missing-docstring)] Missing module docstring

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+"""
+Given a word and a list of possible anagrams, select the correct sublist.
+"""
+
+
 def find_anagrams(word: str, candidates: list):
     """
     Given a word and a list of possible anagrams, select the correct sublist.
