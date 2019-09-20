f1737710560960019e1abe661905d96cd4b777d6
Egor Kostan <igorkostan@gmail.com>
2019-09-19 19:57:57 -0700

    Update phone_number.py
    Method could be a function

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -3,13 +3,13 @@ class Phone(object):
     def __init__(self, phone_number):
         digits_only = ''.join(n for n in phone_number if n.isdigit())
 
-        if self._country_code(digits_only):
-            if self._npa(digits_only):
-                if self._exchange_code(digits_only):
+        if country_code(digits_only):
+            if npa(digits_only):
+                if exchange_code(digits_only):
                     self.number = digits_only \
                         if len(digits_only) == 10 \
                         else digits_only[1:]
-                    self.area_code = self._npa(digits_only)
+                    self.area_code = npa(digits_only)
                 else:
                     raise ValueError('Invalid exchange code')
             else:
@@ -28,35 +28,38 @@ class Phone(object):
                                           self.number[4:7],
                                           self.number[7:])
 
-    # Country code validation:
-    def _country_code(self, digits_only: str):
-        if len(digits_only) == 10:
-            return True
-        elif digits_only[0] == '1' and len(digits_only[1:]) == 10:
-            return True
-        else:
-            return False
 
-    # NPA validation:
-    def _npa(self, digits_only: str):
-        npa = [200, 999]
+# Country code validation:
+def country_code(digits_only: str):
+    if len(digits_only) == 10:
+        return True
+    elif digits_only[0] == '1' and len(digits_only[1:]) == 10:
+        return True
+    else:
+        return False
 
-        if npa[0] <= int(digits_only[0:3]) <= npa[1] \
-                and len(digits_only) == 10:
-            return digits_only[0:3]
-        elif npa[0] <= int(digits_only[1:4]) <= npa[1] \
-                and len(digits_only) == 11:
-            return digits_only[1:4]
-        else:
-            return False
 
-    # Exchange code validation:
-    def _exchange_code(self, digits_only: str):
-        if int(digits_only[3]) > 1 \
-                and len(digits_only) == 10:
-            return True
-        elif int(digits_only[4]) > 1 \
-                and len(digits_only) == 11:
-            return True
-        else:
-            return False
+# NPA validation:
+def npa(digits_only: str):
+    npa = [200, 999]
+
+    if npa[0] <= int(digits_only[0:3]) <= npa[1] \
+            and len(digits_only) == 10:
+        return digits_only[0:3]
+    elif npa[0] <= int(digits_only[1:4]) <= npa[1] \
+            and len(digits_only) == 11:
+        return digits_only[1:4]
+    else:
+        return False
+
+
+# Exchange code validation:
+def exchange_code(digits_only: str):
+    if int(digits_only[3]) > 1 \
+            and len(digits_only) == 10:
+        return True
+    elif int(digits_only[4]) > 1 \
+            and len(digits_only) == 11:
+        return True
+    else:
+        return False
