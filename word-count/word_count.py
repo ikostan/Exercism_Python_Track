dd4f482c0de47528223057c3a225a3d955d8dc4f
Egor Kostan <igorkostan@gmail.com>
2019-11-24 11:50:50 -0800

    Word Count

---
+++ word-count/word_count.py
@@ -0,0 +1,24 @@
+def count_words(sentence: str):
+
+    result = dict()
+    words = filter_illegal_chars(sentence)
+
+    for word in words:
+        if word not in result:
+            result[word] = words.count(word)
+
+    return result
+
+
+def filter_illegal_chars(sentence: str) -> list:
+
+    replaceable = ",:!&@$%^._\n"
+    sentence = ''.join(char if char not in replaceable else " " for char in sentence)
+
+    words = [word.replace(",:!&@$%^.", "").lower() for word in sentence.split()]
+
+    for i, word in enumerate(words):
+        if "'" in word and (word.index("'") == 0 or word.index("'") == -1):
+            words[i] = word.replace("'", "")
+
+    return words
