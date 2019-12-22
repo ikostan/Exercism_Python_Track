1701b230a344aa5f891c95a06503454e95c4cb0d
Egor Kostan <igorkostan@gmail.com>
2019-12-22 13:02:31 -0800

    Update matching_brackets.py
    Matching Brackets

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,2 +1,38 @@
-def is_paired(input_string):
-    pass
+def is_paired(input_string: str) -> bool:
+
+    input_string = clean_input_string(input_string)
+
+    if len(input_string) % 2 != 0:
+        return False
+
+    while input_string:
+        if not remove_bracket(input_string[0], input_string):
+            return False
+
+    return True
+
+
+def remove_bracket(bracket: str, input_string: list) -> bool:
+
+    pairs = {
+        '{': '}',
+        '[': ']',
+        '(': ')',
+    }
+
+    if bracket not in pairs:
+        return False
+
+    if input_string[1] == pairs[bracket]:
+        del input_string[1]
+        del input_string[0]
+        return True
+    elif input_string[-1] == pairs[bracket]:
+        del input_string[-1]
+        del input_string[0]
+        return True
+
+
+def clean_input_string(input_string: str) -> list:
+    template = '[]{}()'
+    return [char for char in input_string if char in template]
