b952b5bd82497d8119264ad352ce1f4e60df888a
Egor Kostan <igorkostan@gmail.com>
2020-01-01 16:16:57 -0800

    Update allergies.py

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,11 +1,39 @@
 class Allergies:
 
     def __init__(self, score):
-        pass
+
+        self._items = {
+            1: 'eggs',
+            2: 'peanuts',
+            4: 'shellfish',
+            8: 'strawberries',
+            16: 'tomatoes',
+            32: 'chocolate',
+            64: 'pollen',
+            128: 'cats',
+        }
+
+        self._score = score % 256
 
     def allergic_to(self, item):
-        pass
+        return item in self.lst
 
     @property
     def lst(self):
-        pass
+
+        allergies = list()
+        score = self._score
+
+        for key in sorted(self._items.keys(), reverse=True):
+
+            if score >= key:
+                score -= key
+                allergies.append(self._items[key])
+
+            if score == 0:
+                break
+
+        return allergies
+
+
+
