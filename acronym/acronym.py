68f74a395f1e6798ed1c778e3be9cc30793b8b8f
Egor Kostan <igorkostan@gmail.com>
2020-01-05 14:50:09 -0800

    Create acronym.py
    Acronym -> done

---
+++ acronym/acronym.py
@@ -0,0 +1,41 @@
+def abbreviate(words: str) -> str:
+    """
+    Convert a phrase to its acronym.
+
+    Techies love their TLA (Three Letter Acronyms)!
+
+    Help generate some jargon by writing a program
+    that converts a long name like Portable Network
+    Graphics to its acronym (PNG).
+    :param words:
+    :return:
+    """
+
+    return ''.join(word[0].upper() for word in clean_up_words(words) if word[0].isalpha())
+
+
+def clean_up_words(words: str) -> list:
+    """
+    Remove garbage from words
+    and return them as a list
+    :param words:
+    :return:
+    """
+
+    results = list()
+
+    for w in words.split(' '):
+        if '-' in w:
+            w = w.replace('-', ' ').split(' ')
+            if w[0] != '':
+                results.append(w[0])
+            if w[1] != '':
+                results.append(w[1])
+        elif '_' in w:
+            w = w.replace('_', '')
+            if w != '':
+                results.append(w)
+        else:
+            if w != '':
+                results.append(w)
+    return results
