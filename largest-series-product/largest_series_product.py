6a926148dd42a9f0852a4b9356002d0b46251247
Egor Kostan <igorkostan@gmail.com>
2020-01-11 14:52:07 -0800

    Update largest_series_product.py
    Largest Series Product > done

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,38 @@
-def largest_product(series, size):
-    pass
+def largest_product(series: str, size: int) -> int:
+    """
+    Given a string of digits, calculate the largest
+    product for a contiguous substring of digits of length n.
+    :param series:
+    :param size:
+    :return:
+    """
+
+    if len(series) < size or size < 0:
+        raise ValueError('ERROR: illegal string.')
+
+    if series == '':
+        return 1
+
+    largest = 0
+    for n in range(0, len(series)):
+        if n + size <= len(series):
+            temp = multiply_list_members(series[n: n + size])
+            if largest < temp:
+                largest = temp
+        else:
+            break
+
+    return largest
+
+
+def multiply_list_members(series: str) -> int:
+    """
+    Multiply all numbers in the list
+    :param series:
+    :return:
+    """
+    result = 1
+    for char in series:
+        result *= int(char)
+    return result
+
