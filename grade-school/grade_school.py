299e7db0de00bb972adcbd89dac291eb22f2209f
Egor Kostan <igorkostan@gmail.com>
2020-01-22 13:56:48 -0800

    Grade School
    Given students' names along with the grade that they are in, create a roster for the school.

---
+++ grade-school/grade_school.py
@@ -0,0 +1,48 @@
+class School:
+    """
+    The roster for the school.
+    Contains given students' names along with the grade that they are in.
+    Note that all our students only have one name.
+    """
+
+    def __init__(self):
+        self._roster = dict()
+
+    def add_student(self, name, grade):
+        """
+        Add a student's name to the roster for a grade
+        :param name:
+        :param grade:
+        :return:
+        """
+        if grade in self._roster:
+            self._roster[grade].append(name)
+            self._roster[grade] = sorted(self._roster[grade])
+        else:
+            self._roster[grade] = [name]
+
+    def roster(self):
+        """
+        Get a sorted list of all students in all grades.
+        Grades should sort as 1, 2, 3, etc., and students
+        within a grade should be sorted alphabetically by name.
+        :return:
+        """
+        grades = sorted(list(self._roster.keys()))
+        names = list()
+        
+        for grade in grades:
+            for name in self._roster[grade]:
+                names.append(name)
+        
+        return names
+
+    def grade(self, grade_number):
+        """
+        Get a list of all students enrolled in a grade
+        :param grade_number:
+        :return:
+        """
+        if grade_number not in self._roster:
+            self._roster[grade_number] = list()
+        return self._roster[grade_number]
