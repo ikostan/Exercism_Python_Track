86a866984b3da60bf205c31b8739611c3ef20d64
Egor Kostan <igorkostan@gmail.com>
2020-02-09 08:39:37 -0800

    Update luhn.py
    Do you think there is an additional value in calling __is_valid_input() twice? What about calling __card_num.reverse() twice?
    
    Personally, I'd try to reduce the amount of unnecessary operations and loops as much as possible.

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -6,13 +6,13 @@ class Luhn:
 
     def __init__(self, card_num: str):
 
-        self.card_num = card_num
+        self.card_num = card_num.replace(' ', '')
 
-        if self.__is_valid_input():
-            self.__card_num = \
-                [int(char) for char in
-                 self.card_num if char.isdigit()]
-            self.__digits_processor()
+        self.__card_num_list = \
+            [int(char) for char in
+             self.card_num if char.isdigit()]
+
+        self.__digits_processor()
 
     # Declaring private method
     def __digits_processor(self):
@@ -23,31 +23,10 @@ class Luhn:
         than 9 then subtract 9 from the product.
         :return:
         """
-
-        self.__card_num.reverse()
-
-        for i, n in enumerate(self.__card_num):
-            if i % 2 != 0:
-                self.__card_num[i] = n * 2
-                if self.__card_num[i] > 9:
-                    self.__card_num[i] = self.__card_num[i] - 9
-
-        self.__card_num.reverse()
-
-    # Declaring private method
-    def __is_valid_input(self) -> bool:
-        """
-        Test if there any garbage
-        (non digit chars) in user input
-        :return:
-        """
-        self.card_num = self.card_num.replace(' ', '')
-
-        for char in self.card_num:
-            if not char.isdigit():
-                return False
-
-        return True
+        for i in range(len(self.__card_num_list) - 2, -1, -2):
+            self.__card_num_list[i] = self.__card_num_list[i] * 2
+            if self.__card_num_list[i] > 9:
+                self.__card_num_list[i] = self.__card_num_list[i] - 9
 
     def valid(self) -> bool:
         """
@@ -57,10 +36,11 @@ class Luhn:
         :return:
         """
 
-        if not self.__is_valid_input():
+        for char in self.card_num:
+            if not char.isdigit():
+                return False
+
+        if len(self.card_num) < 2:
             return False
 
-        if len(self.__card_num) < 2:
-            return False
-
-        return sum(self.__card_num) % 10 == 0
+        return sum(self.__card_num_list) % 10 == 0
