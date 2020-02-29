ce0d63be19077e5ac9ff32b37b6ba20fec7e7a96
Egor Kostan <igorkostan@gmail.com>
2020-02-29 14:01:02 -0800

    Update robot_simulator.py

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -16,12 +16,41 @@ class Robot:
     direction (north, east, south, or west) at a set of {x,y} coordinates,
     e.g., {3,8}, with coordinates increasing to the north and east.
     """
+
+    __valid_directions = (EAST, NORTH, WEST, SOUTH)
+
     def __init__(self, direction=NORTH, x=0, y=0):
-        self.__direction = direction
+
+        self.__set_direction(direction)
+        self.__set_coordinates(x, y)
+
+    def __set_coordinates(self, x: int, y: int) -> None:
+        """
+        Set initial x and y coordinates.
+        Includes data type check.
+        :param x:
+        :param y:
+        :return:
+        """
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise ValueError("Invalid coordinates: x={}, y={}".format(x, y))
         self.__coordinates = {
             'x': x,
             'y': y
         }
+        return None
+
+    def __set_direction(self, direction) -> None:
+        """
+        Set initial direction.
+        Includes data type check.
+        :param direction:
+        :return:
+        """
+        if direction not in self.__valid_directions:
+            raise ValueError("Invalid direction: {}".format(direction))
+        self.__direction = direction
+        return None
 
     @property
     def direction(self) -> tuple:
@@ -45,7 +74,7 @@ class Robot:
         return self.__coordinates['x'], \
                self.__coordinates['y']
 
-    def move(self, movement: str):
+    def move(self, movement: str) -> None:
         """
         Turn left or right or advance.
 
@@ -65,6 +94,10 @@ class Robot:
         :return:
         """
         for m in movement.upper():
+            # Movement check
+            if m not in "LRA":
+                raise ValueError("Invalid movement: {}".format(m))
+
             # Turn RIGHT
             if m == 'R':
                 if self.direction == EAST:
@@ -91,3 +124,5 @@ class Robot:
             if m == 'A':
                 self.__coordinates[self.direction[0]] = eval("{}{}1".format(self.__coordinates[self.direction[0]],
                                                                             self.direction[1]))
+
+        return None
