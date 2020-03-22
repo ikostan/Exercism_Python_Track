a29b2477e19e349e099b1796e6fc25f5ab83f9f8
Egor Kostan <igorkostan@gmail.com>
2020-03-22 12:25:51 -0700

    Update markdown.py

--- markdown/markdown.py
+++ markdown/markdown.py
@@ -6,76 +6,80 @@ def parse(markdown):
     lines = markdown.split('\n')
 
     # Flags
-    res = ''
+    result = ''
     in_list = False
     in_list_append = False
 
     # Process the list line by line and replace
     # patterns into HTML tags
-    for i in lines:
+    for line in lines:
 
-        # Replace hash tag with appropriate header level
-        if '#' in i[:7]:
-            counter = i[:7].count('#')
-            i = '<h{}>'.format(counter) + \
-                i[counter + 1:] + \
-                '</h{}>'.format(counter)
+        line = replace_header(line)
 
-        m = re.match(r'\* (.*)', i)
+        m = re.match(r'\* (.*)', line)
         if m:
 
             curr = m.group(1)
 
-            # Replace double underscore with STRONG tag
-            m1 = re.match('(.*)__(.*)__(.*)', curr)
-            if m1:
-                curr = m1.group(1) + '<strong>' + \
-                       m1.group(2) + '</strong>' + \
-                       m1.group(3)
-
-            # Replace single underscore with italic
-            m1 = re.match('(.*)_(.*)_(.*)', curr)
-            if m1:
-                curr = m1.group(1) + '<em>' + \
-                       m1.group(2) + '</em>' + \
-                       m1.group(3)
-
             if not in_list:
                 in_list = True
-                i = '<ul><li>' + curr + '</li>'
+                line = ''.join(('<ul><li>',
+                                curr,
+                                '</li>'))
             else:
                 # List item
-                i = '<li>' + curr + '</li>'
+                line = ''.join(('<li>',
+                                curr,
+                                '</li>'))
         else:
             if in_list:
                 in_list_append = True
                 in_list = False
 
-        m = re.match('<h|<ul|<p|<li', i)
+        m = re.match('<h|<ul|<p|<li', line)
         if not m:
-            i = '<p>' + i + '</p>'
+            line = '<p>' + line + '</p>'
 
-        # Replace double underscore with STRONG tag
-        m = re.match('(.*)__(.*)__(.*)', i)
-        if m:
-            i = m.group(1) + '<strong>' + \
-                m.group(2) + '</strong>' + \
-                m.group(3)
-
-        # Replace * with EM tag
-        m = re.match('(.*)_(.*)_(.*)', i)
-        if m:
-            i = m.group(1) + '<em>' + \
-                m.group(2) + '</em>' + \
-                m.group(3)
+        line = replace_strong(line)
+        line = replace_em(line)
 
         if in_list_append:
-            i = '</ul>' + i
+            line = '</ul>' + line
             in_list_append = False
 
-        res += i
+        result += line
 
     if in_list:
-        res += '</ul>'
+        result += '</ul>'
 
-    return res
+    return result
+
+
+def replace_em(line: str) -> str:
+    # Replace * with EM tag
+    m = re.match('(.*)_(.*)_(.*)', line)
+    if m:
+        line = ''.join((m.group(1), '<em>',
+                        m.group(2), '</em>',
+                        m.group(3)))
+    return line
+
+
+def replace_strong(line: str) -> str:
+    # Replace double underscore with STRONG tag
+    m = re.match('(.*)__(.*)__(.*)', line)
+    if m:
+        line = ''.join((m.group(1), '<strong>',
+                        m.group(2), '</strong>',
+                        m.group(3)))
+    return line
+
+
+def replace_header(line: str) -> str:
+    # Replace hash tag with appropriate header level
+    if '#' in line[:7]:
+        counter = line[:7].count('#')
+        line = ''.join(('<h{}>'.format(counter),
+                        line[counter + 1:],
+                        '</h{}>'.format(counter)))
+    return line
