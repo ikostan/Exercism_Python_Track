cad985f844706e552c7cded0cc7daa8790b22146
Egor Kostan <igorkostan@gmail.com>
2020-07-24 09:16:25 -0700

    Update simple_cipher.py
    Code refactoring in order to simplify solution

--- simple-cipher/simple_cipher.py
+++ simple-cipher/simple_cipher.py
@@ -1,5 +1,6 @@
 import string
 import secrets
+from itertools import cycle
 
 LOWER_CASE = string.ascii_lowercase
 
@@ -20,47 +21,20 @@ class Cipher:
         return ''.join([LOWER_CASE[secrets.choice(range(0, len(LOWER_CASE)))] for char in text])
 
     def encode(self, text: str) -> str:
-        text: str = text.lower()
         new_text = list()
-
-        if len(text) == len(self.__key):
-            for char, cipher in zip(text, self.__key):
-                index: int = LOWER_CASE.index(char)
-                if index + LOWER_CASE.index(cipher) < len(LOWER_CASE):
-                    new_text.append(LOWER_CASE[index + LOWER_CASE.index(cipher)])
-                else:
-                    new_text.append(LOWER_CASE[(index + LOWER_CASE.index(cipher)) - len(LOWER_CASE)])
-        else:
-            cipher = 0
-            for char in text:
-                index: int = LOWER_CASE.index(char)
-                if index + LOWER_CASE.index(self.__key[cipher]) < len(LOWER_CASE):
-                    new_text.append(LOWER_CASE[index + LOWER_CASE.index(self.__key[cipher])])
-                else:
-                    new_text.append(LOWER_CASE[(index + LOWER_CASE.index(self.__key[cipher])) - len(LOWER_CASE)])
-
-                cipher += 1
-                if cipher >= len(self.__key):
-                    cipher = 0
+        for char, cipher in zip(text.lower(), cycle(self.__key)):
+            index: int = LOWER_CASE.index(char)
+            if index + LOWER_CASE.index(cipher) < len(LOWER_CASE):
+                new_text.append(LOWER_CASE[index + LOWER_CASE.index(cipher)])
+            else:
+                new_text.append(LOWER_CASE[(index + LOWER_CASE.index(cipher)) - len(LOWER_CASE)])
 
         return ''.join(new_text)
 
     def decode(self, text: str) -> str:
-        text: str = text.lower()
         new_text = list()
-
-        if len(text) == len(self.__key):
-            for char, cipher in zip(text, self.__key):
-                index: int = LOWER_CASE.index(char)
-                new_text.append(LOWER_CASE[index - LOWER_CASE.index(cipher)])
-        else:
-            cipher = 0
-            for char in text:
-                index: int = LOWER_CASE.index(char)
-                new_text.append(LOWER_CASE[index - LOWER_CASE.index(self.__key[cipher])])
-
-                cipher += 1
-                if cipher >= len(self.__key):
-                    cipher = 0
+        for char, cipher in zip(text.lower(), cycle(self.__key)):
+            index: int = LOWER_CASE.index(char)
+            new_text.append(LOWER_CASE[index - LOWER_CASE.index(cipher)])
 
         return ''.join(new_text)
