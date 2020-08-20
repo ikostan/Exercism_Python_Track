fdbbd5db25e89cbe0fa74a0042c203ae5c2c962f
Egor Kostan <igorkostan@gmail.com>
2020-08-19 21:12:09 -0700

    Update simple_linked_list.py

--- simple-linked-list/simple_linked_list.py
+++ simple-linked-list/simple_linked_list.py
@@ -18,66 +18,44 @@ class LinkedList:
     """
 
     def __init__(self, values=[]):
-        self.__values = list()
+        self.__head = None
+        self.__len = 0
         for val in values:
-            self.__values.append(Node(val))
-            if len(self.__values) > 1:
-                self.__values[-1].next_node = self.__values[-2]
-
-        self.max = len(self.__values)
+            self.push(val)
 
     def __len__(self):
-        return len(self.__values)
+        return self.__len
 
     def __iter__(self):
-        self.n = 0
         return self
 
     def __next__(self):
-        if self.n < self.max:
-            result = self.__values[-1].value()
-            del self.__values[-1]
-            self.n += 1
-            return result
-        else:
+        try:
+            return self.pop()
+        except EmptyListException:
             raise StopIteration
 
     def head(self):
-        if len(self.__values) == 0:
-            raise EmptyListException("ERROR: the list is empty")
-        return self.__values[-1]
+        if not self.__head:
+            raise EmptyListException('ERROR: the list is empty')
+        return self.__head
 
     def push(self, value):
-        self.__values.append(Node(value=value))
-        if len(self.__values) > 1:
-            self.__values[-1].next_node = self.__values[-2]
-        self.max = len(self.__values)
+        node = Node(value=value)
+        self.__len += 1
+        node.next_node, self.__head = self.__head, node
 
     def pop(self):
-        if len(self.__values) == 0:
+        if self.head():
+            self.__len -= 1
+            val = self.head().value()
+            self.__head = self.head().next()
+            return val
+        else:
             raise EmptyListException("ERROR: the list is empty")
-        val = self.__values[-1]
-        del self.__values[-1]
-        self.max = len(self.__values)
-        return val.value()
 
     def reversed(self):
-        if len(self.__values) == 0:
-            return LinkedList([])
-        elif len(self.__values) == 1:
-            return LinkedList([self.__values[0].value()])
-        else:
-            values: list = list()
-            for node in self.__values:
-                values.append(node.value())
-            values.reverse()
-            return LinkedList(values=values)
-
-    def __repr__(self):
-        nodes = list()
-        for val in self.__values:
-            nodes.append(val.value())
-        return ''.join(nodes)
+        return LinkedList(list(self))
 
 
 class EmptyListException(Exception):
