201446e85947d49e17f062e44f882cf916bbc70f
Egor Kostan <igorkostan@gmail.com>
2020-09-27 12:55:05 -0700

    Update phone_number.py
    remove all the duplicate handling in the pretty() method
    validity_check

--- phone-number/phone_number.py
+++ phone-number/phone_number.py
@@ -2,27 +2,24 @@ class PhoneNumber(object):
 
     def __init__(self, phone_number):
         digits_only = ''.join(n for n in phone_number if n.isdigit())
-
-        if not country_code(digits_only):
-            raise ValueError('Invalid country code')
-        elif not npa(digits_only):
-            raise ValueError('Invalid NPA')
-        elif not exchange_code(digits_only):
-            raise ValueError('Invalid exchange code')
-
+        validity_check(digits_only)
         self.number = digits_only if len(digits_only) == 10 else digits_only[1:]
         self.area_code = npa(digits_only)
 
     # return formatted phone number:
     def pretty(self):
-        if len(self.number) == 10:
-            return '({0})-{1}-{2}'.format(self.number[0:3],
-                                          self.number[3:6],
-                                          self.number[6:])
-        else:
-            return '({0})-{1}-{2}'.format(self.number[1:3],
-                                          self.number[4:7],
-                                          self.number[7:])
+        return '({0})-{1}-{2}'.format(self.number[0:3],
+                                      self.number[3:6],
+                                      self.number[6:])
+
+
+def validity_check(digits_only: str):
+    if not country_code(digits_only):
+        raise ValueError('Invalid country code')
+    elif not npa(digits_only):
+        raise ValueError('Invalid NPA')
+    elif not exchange_code(digits_only):
+        raise ValueError('Invalid exchange code')
 
 
 # Country code validation:
@@ -49,10 +46,8 @@ def npa(digits_only: str):
 
 # Exchange code validation:
 def exchange_code(digits_only: str):
-    if int(digits_only[3]) > 1 \
-            and len(digits_only) == 10:
+    if int(digits_only[3]) > 1 and len(digits_only) == 10:
         return True
-    elif int(digits_only[4]) > 1 \
-            and len(digits_only) == 11:
+    elif int(digits_only[4]) > 1 and len(digits_only) == 11:
         return True
     return False
